cc179aa917de443e9c83bab17e6ea956434a5815
AniketW42 <aniketwakte42@gmail.com>
2026-03-05 14:27:12 +0530

    feat: add responce body validation for refresh and logout using zod

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,8 +5,11 @@ import { z } from "zod";
 const LoginInput = z.object({
   email: z.email(),
   password: z.string()
-})
+});
 
+const TokenInput = z.object({
+  refreshToken: z.string()
+});
 
 export const authController = {
   login: async (req: Request, res: Response) => {
@@ -30,14 +33,14 @@ export const authController = {
   },
 
   refresh: async (req: Request, res: Response) => {
-    const { refreshToken } = req.body;
+    const { refreshToken } = TokenInput.parse(req.body);
     res.status(200).json({
       ...(await authService.refresh(refreshToken)),
     });
   },
 
   logout: async (req: Request, res: Response) => {
-    const { refreshToken } = req.body;
+    const { refreshToken } = TokenInput.parse(req.body);
     await authService.logout(refreshToken);
     res.status(200).json({});
   },
