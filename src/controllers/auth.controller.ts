768c186248a0cf239529324b54fc3ea435fb7eaa
AniketW42 <aniketwakte42@gmail.com>
2026-03-05 12:30:19 +0530

    feat: add responseFormatter Middleware

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -24,7 +24,6 @@ export const authController = {
 
   refresh: async (req: Request, res: Response) => {
     const { refreshToken } = req.body;
-
     res.status(200).json({
       ...(await authService.refresh(refreshToken)),
     });
@@ -33,6 +32,6 @@ export const authController = {
   logout: async (req: Request, res: Response) => {
     const { refreshToken } = req.body;
     await authService.logout(refreshToken);
-    res.status(200);
+    res.status(200).json({});
   },
 };
